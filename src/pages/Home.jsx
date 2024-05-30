4053bb3432398dd35a40ae4bb8123f3a413c81a6
Mkhotamirais <mkhotamirais@gmail.com>
2024-05-30 11:24:56 +0700

    response chat gpt

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,20 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      Home
+      <div>sementara</div>
+      <div>
+        expressing surprice or interest. wow, that's interesting; really, i didn't know that; oh i see, that's new to me;
+      </div>
+      <div>asking for mor information: can you tell me more about it? how does that work? why is that important?</div>
+      <div>showing understanding: i understand now; that make sense; got it, thanks for explaining;</div>
+      <div>giving a positive reaction: thats's great; sounds fun!; that's amazing;</div>
+      <div>
+        making a connection: i have a friend who mentioned something similar; that reminds me of something i read/watched; i
+        thing i saw something like that before.
+      </div>
+    </div>
+  );
 };
 
 export default Home;
